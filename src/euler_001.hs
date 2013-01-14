e56f4a4943692f02ba753abb76ab94a923c857e9
Bjorn Hamre <javaguruen@gmail.com>
2013-01-14 21:59:29 +0100

    Alternativ to

--- src/euler_001.hs
+++ src/euler_001.hs
@@ -2,16 +2,7 @@ module Main where
 import Data.List
 
 euler_001 :: Int -> Int
-euler_001 n =  foldl (+) 0 (unike n)
-
-unike :: Int -> [Int] 
-unike n = nub ( treere (n-1) ++ femmere (n-1) )
-
-treere :: Int  -> [Int]
-treere n = [3,6 .. n]
-
-femmere :: Int  -> [Int]
-femmere n = [5,10 .. n]
+euler_001 n =  foldl (+) 0 ( filter (\x -> mod x 3 == 0 || mod x 5 == 0) [1 .. n-1] )
 
 riktig :: Bool
 riktig = euler_001 (1000) == 233168
